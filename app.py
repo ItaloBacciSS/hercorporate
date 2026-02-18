6ac907e20354e3bcd3df4453b3d1f69ff4408a6c
Italo Bacci <italo.bacci18@gmail.com>
2026-02-18 14:53:10 -0300

    fix render port

--- app.py
+++ app.py
@@ -99,9 +99,9 @@ def salvar_progresso_modulo_route():
 
 
 # ---------------- RUN ---------------- #
-import os
-
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
